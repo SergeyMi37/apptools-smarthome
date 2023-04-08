8a169692c2d9660b6b4a4d5f4c04cee65104569a
SergeyMi37 <Sergey.Mikhaylenko@gmail.com>
2023-04-08 13:35:50 +0300

    upd py

--- python/check-send.py
+++ python/check-send.py
@@ -1,4 +1,4 @@
-# A program to regularly read the temperature sensor and send a message to the telebot and the iris server
+# Программа регулярного чтения датчика температуры и посылки сообщения телеботу
 #pip install requests
 #pip install --upgrade pip
 import requests
@@ -6,10 +6,14 @@ import threading
 import datetime
 import yaml
 import socket
+import sys
 
-with open('/home/pi/repo/new-test/check-send.yml') as _fi:
+_path_file_conf = sys.argv[1]
+with open(_path_file_conf) as _fi:
     _param = yaml.safe_load(_fi)
 print(_param["token"])
+_time=int(_param["timeout"])
+_time_dflt=_time
 
 
 def send_telegram(text: str):
@@ -25,45 +29,62 @@ def send_telegram(text: str):
 
     if r.status_code != 200:
         #raise Exception("post_text error")
-        print("Error ",r.status_code)
+        print("Ошибка",r.status_code)
     else:
-        print("sent successfully")
+        print("Послано удачно")
+
 
 def read_tempera():
-    threading.Timer(_param["timeout"], read_tempera).start()  # Restart after 3600 seconds - every hour
-    _dt=str(datetime.datetime.today().strftime("%Y-%m-%d_%H.%M"))
     tfile=open(_param["dirbus1w"])
     ttext=tfile.read()
     tfile.close()
-    temp=ttext.split("\n")[1].split(" ")[9]
-    _temp=float(temp[2:])/1000
+    te=ttext.split("\n")[1].split(" ")[9]
+    return float(te[2:])/1000
+
+
+def check_tempera():
+    global _time,_time_dflt
+    threading.Timer(_time, check_tempera).start()  # Перезапуск через 3600 секунд - каждый час
+    _temp=read_tempera()
     _msg="👉 температура "+str(_temp)
+    _dt=str(datetime.datetime.today().strftime("%Y-%m-%d_%H.%M"))
 
     #print(_temp)
     if _temp < _param["min_threshold"]:
-        _msg=" 🚨🚨🚨🚨🚨 Attention limit lower temperature threshold "+str(_temp)
+        _msg=" 🚨🚨🚨🚨🚨 Внимание предельный нижний порог темпратуры "+str(_temp)
         send_telegram(_msg)
+        _time=60
     elif _temp > _param["max_threshold"]:
-        _msg=" 🚨🚨🚨🚨🚨 Attention upper temperature limit "+str(_temp)
+        _msg=" 🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨 Внимание предельный верхний порог темпратуры "+str(_temp)
         send_telegram(_msg)
-    elif _param["debug_print"]:
+        _time=60
+    elif _param["dubug_print"]:
         send_telegram(_msg)
-    print(_dt+_msg)
+        _time=_time_dflt
 
+    print(_dt+_msg)
+    _ret=save_db(_dt,_msg,_temp)
+    print(_ret)
+
+
+def save_db(_dt,_msg,_temp):
     _username=_param["username"]
     _password=_param["password"]
-    _base_url=_param["base_url"]+msg
+    _base_url=_param["base_url"]+_dt+_msg
     #print(_param)
 
     headers = {'Accept': 'application/json;odata=verbose'}
     responce = requests.get(_base_url,verify=False,auth=(_username,_password),headers=headers)
 
     respon_list=responce.json()
-    print(respon_list)
+    #print(respon_list)
+    return respon_list
 
 
 if __name__ == '__main__':
-    msg=" ✅ Старт мониторинга температурного датчика. Периодичность: "+str(_param["timeout"])+", пороги оповещения: "+str(_param["min_threshold"])+" <> "+str(_param["max_threshold"])
+    msg=" ✅ Старт мониторинга температурного датчика. Периодичность: "+str(_param["timeout"])+", пороги оповещения: "+str(_param["min_threshold"])+" "+str(_param["max_threshold"])
     send_telegram(msg+ " "+socket.gethostname()+" "+str(socket.gethostbyname(socket.gethostname())))
-    print(msg)
-    read_tempera()
+    send_telegram("Текущая температура "+str(read_tempera()))
+    _ret=save_db("",msg,"")
+    print(_ret,msg)
+    check_tempera()
